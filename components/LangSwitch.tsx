6e849183ff133a7958bd4e73bd7bec8f5081d8f2
Ruxshona2804 <rukhshona.umidjonovnaa@gmail.com>
2025-06-03 21:57:27 +0500

    apos

--- components/LangSwitch.tsx
+++ components/LangSwitch.tsx
@@ -24,7 +24,7 @@ const LangSwitch: React.FC = () => {
     <div>
       <select onChange={switchHandle} value={currentLocale}>
         <option value="en">English</option>
-        <option value="uz">O&aposzbek</option>
+        <option value="uz">O&apos;zbek</option>
       </select>
     </div>
   );
